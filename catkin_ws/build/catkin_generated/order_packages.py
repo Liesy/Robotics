ad9325ee48c541617d6982433379e885ab7d1002
Uzi0603 <preposterous@163.com>
2021-05-24 11:20:07 +0800

    5.24

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ly/桌面/Robotics/catkin_ws/src'
+whitelisted_packages = 'hello_world'.split(';') if 'hello_world' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ly/桌面/Robotics/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ly/桌面/Robotics/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/hello_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ly/桌面/Robotics/catkin_ws/src/hello_world
+# Build directory: /home/ly/桌面/Robotics/catkin_ws/build/hello_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
