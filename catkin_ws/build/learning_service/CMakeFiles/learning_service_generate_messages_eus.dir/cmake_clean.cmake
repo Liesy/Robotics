4e9dd3785ffd9bfd358aadc94336fa118022272e
Uzi0603 <preposterous@163.com>
2021-06-07 11:12:35 +0800

    6.7 finished

---
+++ catkin_ws/build/learning_service/CMakeFiles/learning_service_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ly/桌面/Robotics/catkin_ws/devel/share/roseus/ros/learning_service/manifest.l"
+  "/home/ly/桌面/Robotics/catkin_ws/devel/share/roseus/ros/learning_service/srv/Person.l"
+  "CMakeFiles/learning_service_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/learning_service_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
